46c0fd322de492f68d17950d0ca2c265b82c9629
Eduardo <eduardo.milione8@gmail.com>
2026-05-19 19:07:50 -0300

    feat: implementa classe Atendimento

---
+++ src/main/model/Atendimento.java
@@ -0,0 +1,76 @@
+package model;
+
+import decorator.ServicoVeterinario;
+import observer.Observador;
+import state.Agendado;
+import state.EstadoAtendimento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Atendimento {
+
+    private Tutor tutor;
+    private Animal animal;
+    private EstadoAtendimento estado;
+    private ServicoVeterinario servico;
+
+    private List<Observador> observadores =
+            new ArrayList<>();
+
+    public Atendimento(
+            Tutor tutor,
+            Animal animal,
+            ServicoVeterinario servico
+    ) {
+
+        this.tutor = tutor;
+        this.animal = animal;
+        this.servico = servico;
+
+        this.estado = new EstadoAtendimento();
+    }
+
+    public void adicionarObservador(
+            Observador observador
+    ) {
+
+        observadores.add(observador);
+    }
+
+    public void notificar(String mensagem) {
+
+        for (Observador observador
+                : observadores) {
+
+            observador.atualizar(mensagem);
+        }
+    }
+
+    public void iniciar() {
+        estado.iniciar(this);
+    }
+
+    public void finalizar() {
+        estado.finalizar(this);
+    }
+
+    public void cancelar() {
+        estado.cancelar(this);
+    }
+
+    public void setEstado(
+            EstadoAtendimento estado
+    ) {
+
+        this.estado = estado;
+    }
+
+    public double getValorFinal() {
+        return servico.getValor();
+    }
+
+    public String getEstadoAtual() {
+        return estado.getNome();
+    }
+}
