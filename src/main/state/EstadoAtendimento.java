2f797c99cbbe41a11004e8c8931970eb35557d84
Eduardo <eduardo.milione8@gmail.com>
2026-05-19 19:11:12 -0300

    feat: cria interface EstadoAtendimento

---
+++ src/main/state/EstadoAtendimento.java
@@ -0,0 +1,20 @@
+package state;
+
+import model.Atendimento;
+
+public interface EstadoAtendimento {
+
+    void iniciar(
+            Atendimento atendimento
+    );
+
+    void finalizar(
+            Atendimento atendimento
+    );
+
+    void cancelar(
+            Atendimento atendimento
+    );
+
+    String getNome();
+}
